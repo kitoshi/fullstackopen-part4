6490f1b6c9f11cecb5bd19d641495bd35541995f
kitoshi <kitoshi.charlton@gmail.com>
2021-04-27 10:38:19 -0700

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,55 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://fullstack:nBsiTpwVtSXmQy7@cluster0.4qsoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.4qsoy.mongodb.net/person-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const persons = new Person({
+    id: Person.length + 1,
+    name: process.argv[3],
+    number: process.argv[4]
+})
+
+if (process.argv.length > 3) {
+  persons.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+}
+
+
+if (process.argv.length < 4) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })  
+}
+
+
