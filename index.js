eaa0b1904932420e7e43379a378e025d9243be1e
kitoshi <kitoshi.charlton@gmail.com>
2021-04-28 11:51:38 -0700

    improved4

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const Person = require('./models/person')
 
 
 app.use(cors())
-
+/*
 app.use(express.json())
+*/
 
 /*
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
