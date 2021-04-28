404fd8a024281a2d4306035327acdd73fc3568c3
kitoshi <kitoshi.charlton@gmail.com>
2021-04-28 16:21:33 -0700

    3.22: Lint configuration

--- models/person.js
+++ models/person.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(result => {
+  .then(result => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -33,11 +33,11 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-personSchema.plugin(uniqueValidator);
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+personSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('Person', personSchema)
